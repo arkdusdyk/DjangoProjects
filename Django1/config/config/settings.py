43768a2a9e7f58519a46399ea75d2bd8088c4187
arkdusdyk <arkdusdyk@gmail.com>
2020-12-29 00:49:34 +0900

    SECRET_KEY separation

--- Django1/config/config/settings.py
+++ Django1/config/config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os, json
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'e@^#^q%njy!a_e*03xg$+5=zx(2x-7q7pragkh$sucl^+j-+7@'
+secret_file = os.path.join(BASE_DIR, 'secrets.json')
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+def get_secret(setting, secrets=secrets):
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+SECRET_KEY = get_secret("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- Django1/readme.txt
+++ Django1/readme.txt
@@ -1,2 +1,5 @@
+20.12.28
 first Django : run server
 + viewing polls
+20.12.29
+separate SECRET_KEY
