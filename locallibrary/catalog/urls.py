255ea1fc4160a38bb0e1eb09cd5abf2913a5bb1f
arkdusdyk <arkdusdyk@gmail.com>
2021-01-07 10:33:02 +0900

    local library basic

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from catalog import views
 
 urlpatterns = [
-    path('',views.index,name='index'),
+    path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'), #URL mapper ex) <uuid:ppk> : special formatted string to capture book id, pk is the parameter to pass to view, we can also use regular expressions to express advanced path matching
+    path('authors/', views.AuthorListView.as_view(), name='authors'),
+    path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
 ]
--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -23,3 +23,29 @@ def index(request):
     }
     return render(request, 'index.html',context=context)
     #rendering HTML template index.html with data in context variable
+
+from django.views import generic
+class BookListView(generic.ListView):
+    model = Book
+    '''
+    get_query_set() methd override-> change record's list
+    def get_queryset(self):
+        return Book.objects.filter(title__icontains='Data')[:5] #Get 5 books containing the title Data
+
+    or we could override get_context_data() -> pass extra context parameters at template
+    def get_context_data(self, **kwargs):
+        context = super(BookListView, self).get_contect_data(**kwargs)
+        context['some_data'] = 'This is just some data'
+        return context
+    #follows certain patterns 1. Bring original context from super class 2.Add new context info 3. Return updated context
+    '''
+    paginate_by = 4    #pagination : if you get 10 or more records, view will start pagination, we can access page2 by using URL = /catalog/books/?page=2
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 4
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
